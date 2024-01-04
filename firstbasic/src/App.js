930aaf139d16224b621d4d110ce0f4a7447f9b2b
himanshu30j <himanshumittal30j@gmail.com>
2024-01-04 12:01:19 +0530

    created project via react and via vite

---
+++ firstbasic/src/App.js
@@ -0,0 +1,10 @@
+
+
+
+function App() {
+  return (
+   <h1>himanshu mittal </h1>
+  );
+}
+
+export default App;
---
+++ firstbasicwithvite/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+
+
+
+function App() {
+
+
+  return (
+    <>
+      <h1>himanshu mittal with vite </h1>
+    </>
+  )
+}
+
+export default App
